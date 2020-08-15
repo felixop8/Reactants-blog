bf6d26219a071b6ab52d7154d2620221405348a0
Felix Lopez <felix.lopez@vacasa.com>
2020-08-14 18:16:51 -0700

    Changes html tag for title

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,10 +9,11 @@ const Layout = ({ location, title, children }) => {
 
   if (location.pathname === rootPath) {
     header = (
-      <h1
+      <h2
         style={{
           ...scale(1.5),
           marginBottom: rhythm(1.5),
+          fontFamily: `Montserrat, sans-serif`,
           marginTop: 0,
         }}
       >
@@ -25,7 +26,7 @@ const Layout = ({ location, title, children }) => {
         >
           {title}
         </Link>
-      </h1>
+      </h2>
     )
   } else {
     header = (
